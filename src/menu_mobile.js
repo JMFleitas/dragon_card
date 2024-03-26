02846a9be1287c53ed9b2f4ae62442c2b6b58168
Jonathan Fleitas <marcelofolk17@gmail.com>
2024-03-25 22:56:20 -0300

    Se creo el fotter con datos del programador, se agrego sonido a los botones de menu mobile ,pagination y search, se cambio ancho de cards

--- src/menu_mobile.js
+++ src/menu_mobile.js
@@ -1,16 +1,24 @@
-export default function menuMobile(ctaMenu,navLink,menuMobile) {
-    document.addEventListener("click", e=>{
-        const imgBtn = document.querySelector(".btn-img")
-        if(e.target.matches(ctaMenu)||e.target.matches(`${ctaMenu} *`)){
-            document.querySelector(menuMobile).classList.toggle("menu-active");
-            imgBtn.classList.toggle("rotate-img")  
-            document.body.classList.toggle("no-scroll"); 
+export default function menuMobile(ctaMenu, navLink, menuMobile) {
+    document.addEventListener("click", e => {
+        const imgBtn = document.querySelector(".btn-img"),
+            $menuMobile = document.querySelector(menuMobile),
+            $soundMenuMb = document.createElement("audio");
+        $soundMenuMb.src = "../assets/sound-menu.mp3";
+
+        if (e.target.matches(ctaMenu) || e.target.matches(`${ctaMenu} *`)) {
+            $menuMobile.classList.toggle("menu-active");
+            imgBtn.classList.toggle("rotate-img")
+            document.body.classList.toggle("no-scroll");
+
+            if ($menuMobile.classList.contains("menu-active")) {
+                $soundMenuMb.play()
+            }
         }
-        if(e.target.matches(navLink)){
-            document.querySelector(menuMobile).classList.remove("menu-active"); 
-            imgBtn.classList.toggle("rotate-img--reverse");     
-            imgBtn.classList.toggle("rotate-img");   
-            document.body.classList.toggle("no-scroll"); 
+        if (e.target.matches(navLink)) {
+            document.querySelector(menuMobile).classList.remove("menu-active");
+            imgBtn.classList.toggle("rotate-img--reverse");
+            imgBtn.classList.toggle("rotate-img");
+            document.body.classList.toggle("no-scroll");
         }
     })
 }
